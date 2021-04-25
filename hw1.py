af4b6f92b3f638607ae8ac1c7a2ee812e7f6efa7
Zekihan AZMAN <zekihanazman99@gmail.com>
2021-04-25 19:24:46 +0300

    pytorch start

--- hw1.py
+++ hw1.py
@@ -4,6 +4,21 @@ import re
 import numpy as np
 from gensim.models import word2vec
 
+import torch
+from torchtext import data as tdata
+
+SEED = 2019
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+TEXT = tdata.Field(tokenize='spacy', batch_first=True, include_lengths=True)
+LABEL = tdata.LabelField(dtype=torch.float, batch_first=True)
+fields = [(None, None), ('text', TEXT), ('label', LABEL)]
+# loading custom dataset
+training_data = tdata.TabularDataset(path='quora.csv', format='csv', fields=fields, skip_header=True)
+
+# print preprocessed text
+print(vars(training_data.examples[0]))
+
 stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
               "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
               "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
@@ -26,7 +41,11 @@ embedding_size = 200
 hidden_layer_size = 2
 learning_rate = 0.5
 number_of_epochs = 20
-path = "./data"  # please use relative path like this
+path = "./data"
+
+n_output_class = 2
+n_inputs = 200 * 200
+n_outputs = output_size * n_output_class
 
 
 def activation_function(weights, layer):
@@ -52,8 +71,18 @@ def sigmoid(layer):
     return value
 
 
-def derivation_of_loss_function(true_labels):
-    errors_list = []
+def forward_pass(network, data):
+    inputs = data
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            neuron['output'] = activation_function(neuron['weights'], inputs)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+
+def backward_pass(network, expected):
     for i in reversed(range(len(network))):
         layer = network[i]
         errors = list()
@@ -66,33 +95,10 @@ def derivation_of_loss_function(true_labels):
         else:
             for j in range(len(layer)):
                 neuron = layer[j]
-                errors.append(true_labels[j] - neuron['output'])
-        errors_list.append(errors)
-    return errors_list
-
-
-# the derivation should be with respect to the output neurons
-
-def forward_pass(network, data):
-    inputs = data
-    for layer in network:
-        new_inputs = []
-        for neuron in layer:
-            neuron['output'] = activation_function(neuron['weights'], inputs)
-            new_inputs.append(neuron['output'])
-        inputs = new_inputs
-    return inputs
-
-
-# [hidden_layers] is not an argument, but it is up to you how many hidden layers to implement.
-# so replace it with your desired hidden layers
-
-def backward_pass(network, loss):
-    for i in reversed(range(len(network))):
-        layer = network[i]
+                errors.append(expected[j] - neuron['output'])
         for j in range(len(layer)):
             neuron = layer[j]
-            neuron['delta'] = loss[j] * derivation_of_activation_function(neuron['output'])
+            neuron['delta'] = errors[j] * derivation_of_activation_function(neuron['output'])
 
 
 def update_weights(network, row, l_rate):
@@ -106,36 +112,34 @@ def update_weights(network, row, l_rate):
             neuron['weights'][-1] += l_rate * neuron['delta']
 
 
-def train(dataset, valid_dataset):
+def train(network, dataset, valid_dataset):
     global loss
     for epoch in range(number_of_epochs):
         index = 0
-        # Same thing about [hidden_layers] mentioned above is valid here also
+        loss = 0
         for data in dataset:
             predictions = forward_pass(network, data)
             expected = [0 for i in range(n_outputs)]
             expected[int(data[-1])] = 1
-            loss_signals = derivation_of_loss_function(expected)
-            backward_pass(network, loss_signals)
-            loss = loss_function(expected, predictions)
+            backward_pass(network, expected)
+            loss += loss_function(expected, predictions)
             update_weights(network, data, learning_rate)
 
-            if index % 2000 == 0:  # at each 2000th sample, we run validation set to see our model's improvements
-                accuracy, loss = test(valid_dataset)
+            if index % 200 == 0:
+                accuracy, loss = test(network, valid_dataset)
                 print("Epoch= " + str(epoch) + ", Coverage= %" + str(
-                    100 * (index / len(data))) + ", Accuracy= " + str(accuracy) + ", Loss= " + str(loss))
+                    100 * (index / len(dataset))) + ", Accuracy= " + str(accuracy) + ", Loss= " + str(loss))
 
             index += 1
 
     return loss
 
 
-def test(test_dataset):
+def test(network, test_dataset):
     global data
     avg_loss = 0
     predictions = []
     labels = []
-
     for data in test_dataset:  # Turns through all data
         prediction = forward_pass(network, data)
         predictions.append(prediction)
@@ -179,8 +183,6 @@ def word2vec_init():
     data_text = get_file_data('./data/reviews.txt')
     data_text.append(['<padding>', '<unknown>'])
     model = word2vec.Word2Vec(data_text, vector_size=200, window=5, min_count=1, workers=4)
-    model.save("word2vec.model")
-    model = word2vec.Word2Vec.load("word2vec.model")
     model.train(data_text, total_examples=len(data_text), epochs=10)
     return model
 
@@ -205,20 +207,23 @@ def initialize_network(n_inputs, n_hidden, n_outputs):
     return network
 
 
-def vectorize_data(model, data_text):
+def vectorisation_data(model, data_text):
     data_text = np.asarray(data_text)
     new_data = []
     for text in data_text:
         new_text = []
         for word in text[:200]:
-            new_text.append(model.wv[word])
+            try:
+                new_text.append(model.wv[word])
+            except:
+                new_text.append(model.wv['<unknown>'])
         for _ in range(len(new_text), 200):
             new_text.append(model.wv['<padding>'])
         new_data.append(np.asarray(new_text).flatten())
     return new_data
 
 
-if __name__ == "__main__":
+def part_a():
     model = word2vec_init()
 
     data_text = get_file_data('./data/reviews.txt')
@@ -226,12 +231,13 @@ if __name__ == "__main__":
     labels = generate_training_label(labels_text)
 
     train_x, train_y = np.asarray(data_text[0:int(0.1 * len(data_text))]), np.asarray(labels[0:int(0.1 * len(labels))])
-    test_x, test_y = np.asarray(data_text[int(0.8 * len(data_text)):-1]), np.asarray(labels[int(0.8 * len(labels)):-1])
+    test_x, test_y = vectorisation_data(model, np.asarray(data_text[int(0.8 * len(data_text)):-1])), np.asarray(
+        labels[int(0.8 * len(labels)):-1])
 
     # Training and validation split. (%80-%20)
-    valid_x = np.asarray(train_x[int(0.8 * len(train_x)):-1])
+    valid_x = vectorisation_data(model, np.asarray(train_x[int(0.8 * len(train_x)):-1]))
     valid_y = np.asarray(train_y[int(0.8 * len(train_y)):-1])
-    train_x = np.asarray(train_x[0:int(0.8 * len(train_x))])
+    train_x = vectorisation_data(model, np.asarray(train_x[0:int(0.8 * len(train_x))]))
     train_y = np.asarray(train_y[0:int(0.8 * len(train_y))])
 
     train_dataset = np.concatenate((train_x, train_y), axis=1)
@@ -240,8 +246,18 @@ if __name__ == "__main__":
 
     n_inputs = len(train_dataset[0]) - 1
     n_outputs = len(set([row[-1] for row in train_dataset]))
-    network = initialize_network(n_inputs, 2, n_outputs)
+    network = initialize_network(n_inputs, hidden_layer_size, n_outputs)
 
-    train(train_dataset, valid_dataset)
+    train(network, train_dataset, valid_dataset)
     print("Test Scores:")
-    print(test(test_dataset))
+    print(test(network, test_dataset))
+
+
+def part_b():
+    data_text = get_file_data('./data/reviews.txt')
+    labels_text = get_file_data('./data/labels.txt')
+
+
+if __name__ == "__main__":
+    # part_a()
+    part_b()
